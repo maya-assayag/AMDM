9654be7eb2ccc376bdd7103a6a1b2f43b50a8d2d
Maor <Maor@MAOR-PC>
2021-08-20 14:10:03 +0300

    fix bug on twiiter+create RSS sport news page

---
+++ AMDM/Controllers/RSSFeedController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using AMDM.Models;
+using System.Net;
+using System.Xml.Linq;
+
+namespace AMDM.Controllers
+{
+    public class RSSFeedController : Controller
+    {
+        public IActionResult Index()
+        {
+            const string RSSURL = "https://rss.nytimes.com/services/xml/rss/nyt/Sports.xml";
+            WebClient wclient = new WebClient();
+            string RSSData = wclient.DownloadString(RSSURL);
+
+            XDocument xml = XDocument.Parse(RSSData);
+            var RSSFeedData = (from x in xml.Descendants("item")
+                               select new RSSFeed
+                               {
+                                   Title = ((string)x.Element("title")),
+                                   Link = ((string)x.Element("link")),
+                                   PubDate = ((string)x.Element("pubDate"))
+                               });
+            ViewBag.RSSFeed = RSSFeedData;
+            ViewBag.URL = RSSURL;
+            return View();
+        }
+    }
+}
---
+++ AMDM/Models/RSSFeed.cs
@@ -0,0 +1,8 @@
+﻿namespace AMDM.Models
+{
+    public class RSSFeed
+    {   public string Title { get; set; }
+        public string Link { get; set; }
+        public string PubDate { get; set; }
+    }
+}

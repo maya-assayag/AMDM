dc6d479a88c6557713c468aa8f9fe77e26be2f1d
Maya Assayag <maya.assyag@gmail.com>
2021-08-24 21:21:53 +0300

    fix twitter refresh

--- AMDM/wwwroot/js/twitter.js
+++ AMDM/wwwroot/js/twitter.js
@@ -1,7 +1,31 @@
 ﻿$(function () {
     $('#tweet-submit').submit(function () {
 
-        $('#all-tweets').load();
+        /*$('#all-tweets').load();*/
+
+
+
+
+        $.ajax({
+            url: '/Home/GetAllTweets',
+        }).done(function (data) {
+            console.log(data);
+
+
+            var template = $('#hidden-template-trainings-types-filter').html();
+
+            $.each(data, function (i, val) {
+
+                var temp = template;
+
+                $.each(val, function (key, value) {
+                    temp = temp.replaceAll('{' + key + '}', value);
+                });
+
+                $('#type-filter').append(temp);
+
+            });
+        });
     });
        
 });

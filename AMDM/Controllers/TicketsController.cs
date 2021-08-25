4478bf4d56a5d07de4f5013d2712623395ded78b
Maya Assayag <maya.assyag@gmail.com>
2021-08-25 18:42:24 +0300

    ticket search

--- AMDM/Controllers/TicketsController.cs
+++ AMDM/Controllers/TicketsController.cs
@@ -22,6 +22,60 @@ namespace AMDM.Controllers
             _service = service;
             _context = context;
         }
+        public async Task<IActionResult> Search(string query, string traineeId, string purchaseDateFilter, string expiredDateFilter)
+        {
+            /*var*/
+            IQueryable<Ticket> aMDMContext = _context.Ticket
+                .Include(t => t.TicketType)
+                .Include(t => t.Trainee)
+                .Where(t => (query == null
+                        || t.Trainee.FirstName.Contains(query)
+                        || t.Trainee.LastName.Contains(query)
+                        || t.Trainee.Id.Contains(query)
+                        || t.TicketType.Name.Contains(query)));
+
+            if (traineeId != null)
+            {
+                aMDMContext = aMDMContext.Where(t => t.Trainee.Id.Equals(traineeId));
+            }
+           
+            if (purchaseDateFilter == "today")
+            {
+                aMDMContext.Where(ticket => ticket.PurchaseDate == DateTime.Now.Date);
+            }
+            if (purchaseDateFilter == "tomorrow")
+            {
+                aMDMContext.Where(ticket => ticket.PurchaseDate == DateTime.Now.Date.AddDays(1));
+            }
+            if (purchaseDateFilter == "week")
+            {
+                aMDMContext.Where(ticket => (ticket.PurchaseDate >= DateTime.Now.Date && ticket.PurchaseDate <= DateTime.Now.Date.AddDays(7)));
+            }
+
+
+            if (expiredDateFilter == "today")
+            {
+                aMDMContext.Where(ticket => ticket.ExpiredDate == DateTime.Now.Date);
+            }
+            if (expiredDateFilter == "tomorrow")
+            {
+                aMDMContext.Where(ticket => ticket.ExpiredDate == DateTime.Now.Date.AddDays(1));
+            }
+            if (expiredDateFilter == "week")
+            {
+                aMDMContext.Where(ticket => (ticket.ExpiredDate >= DateTime.Now.Date && ticket.ExpiredDate <= DateTime.Now.Date.AddDays(7)));
+            }
+            
+            var q = from t in aMDMContext
+                        //orderby t.Date 
+                    select new NewRecord(t.TicketType.Name, t.Id, t.Trainee.FirstName, t.Trainee.Id, t.RemainingPunchingHoles, t.PurchaseDate, t.ExpiredDate);
+
+
+            //return View("Index", await aMDMContext.ToListAsync()); //NOT WORK
+
+            return Json(await q.ToListAsync());
+
+        }
 
         // GET: Tickets
         public async Task<IActionResult> Index()
@@ -168,4 +222,6 @@ namespace AMDM.Controllers
             return _context.Ticket.Any(e => e.Id == id);
         }
     }
+
+    internal record NewRecord(string Name, int Id, string FirstName, string Item, int RemainingPunchingHoles, DateTime PurchaseDate, DateTime ExpiredDate);
 }
---
+++ AMDM/wwwroot/js/ticket-search.js
@@ -0,0 +1,55 @@
+﻿$(function () {
+
+
+    $('form').on('submit',function (e) {
+        e.preventDefault();
+        var query = $('#query').val();
+
+        var traineeId = $('#trainee-id-query').val();
+       
+        var selectPurchaseDate = document.getElementById('purchase-date-filter');
+        var purchaseDateFilter = selectPurchaseDate.options[selectPurchaseDate.selectedIndex].value;
+
+        var selectExpiredDate = document.getElementById('expired-date-filter');
+        var expiredDateFilter = selectExpiredDate.options[selectExpiredDate.selectedIndex].value;
+
+        
+
+
+        $.ajax({
+            //method :'post',
+            url: '/Tickets/Search',
+            data: {
+                'query': query,
+                'traineeId': traineeId,
+                'purchaseDateFilter': purchaseDateFilter,
+                'expiredDateFilter': expiredDateFilter
+            }
+        }).done(function (data) {
+
+                    $('tbody').html('');
+
+                    var template = $('#hidden-template-search-resulte').html();
+
+                    $.each(data, function (i, val) {
+
+                        var temp = template;
+
+                        console.log(val);
+
+                        $.each(val, function (key, value) {
+                            temp = temp.replaceAll('{' + key + '}', value);
+                        });
+
+                        $('tbody').append(temp);
+
+                    });
+
+
+
+                });
+
+
+
+    });
+});

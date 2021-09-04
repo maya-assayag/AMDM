993d449180eafecaf3abbf83f56f9f8510b5da03
Maya Assayag <maya.assyag@gmail.com>
2021-09-04 13:35:44 +0300

    modal example

--- AMDM/wwwroot/js/double-arrow-button.js
+++ AMDM/wwwroot/js/double-arrow-button.js
@@ -7,3 +7,12 @@ $(".landing-page-arrow").click(function (e) {
     var url = 'https://localhost:44361/TrainingTypes/Index';
     $(location).prop('href', url);
 });
+
+$(".admin-page-double-arrow").click(function (e) {
+    $('#exampleModal').modal();
+});
+
+$("#close-modal-double-arrow").click(function (e) {
+    $('#exampleModal').remove();
+    location.reload();
+});
